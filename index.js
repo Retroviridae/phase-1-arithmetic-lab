dfba4cf704d43b1af4066db7d4744e2f23e81e1d
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-05 08:20:05 -0500

    commit tree a331ebd2ce9afbf9504aefe25df4468bb9ebd148

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2
 
-// Write your code here
+let random = Math.ceil(Math.random()*10)
+
+let num3 = 10
+let num4 = 6
+let mod = num3%num4
+
+let max = Math.max (1,2,3,4,20);
